284e5e62bc3a4fa5a0cb267667ab1ddda9a5a207
luciob <lucio.bordonaro@gmail.com>
2024-09-18 09:42:38 +0200

    test: Added play test to TabList

--- src/components/TabList/index.stories.tsx
+++ src/components/TabList/index.stories.tsx
@@ -1,3 +1,4 @@
+import { expect, fireEvent, fn, within } from "@storybook/test";
 import TabList, { TabItemProps } from ".";
 import { Meta, StoryObj } from "@storybook/react";
 
@@ -9,6 +10,8 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const onClickMock = fn((id: string) => console.log(`Tab ${id} clicked`));
+
 const tabs: TabItemProps[] = [
   { id: "emails", label: "Emails", selected: true },
   { id: "files", label: "Files", badge: { label: "Warning", variant: "negative" } },
@@ -19,10 +22,27 @@ const tabs: TabItemProps[] = [
 
 export const Primary: Story = {
   args: {
-    onClick: (id: string) => console.log(id),
+    onClick: onClickMock,
     selectedTab: "emails",
     tabs,
     variant: "pill",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const tabList = canvas.getByRole("tablist");
+    if (!tabList) {
+      return;
+    }
+
+    const tabId = Math.ceil(Math.random() * tabs.length);
+    const tab = canvas.getAllByRole("tab")[tabId];
+    if (!tab) {
+      return;
+    }
+
+    fireEvent.click(tab);
+    await expect(onClickMock).toHaveBeenCalledTimes(onClickMock.mock.calls.length);
+    await expect(onClickMock).toHaveBeenCalledWith(tabs[tabId].id);
   },
 };
 
